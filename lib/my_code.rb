464289c0e68e6f7e5086fd39ff04433a9def94a0
Tariq Bailey <tariqbailey627@gmail.com>
2019-09-24 23:55:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# Your Code Here
+def map (source_array)
+ i = 0 
+ negative_array = []
+ while i < source_array.count do
+  negative_array << yield(source_array[i])
+  i += 1 
+ end
+negative_array
+end
+
+def reduce(source_array)
+  i = 0 
+  total = 0
+  while source_array.size > i do
+    total << yield(source_array[i])
+    total = total + source_array[i]
+    i += 1 
+  end
+end
+    
+  
