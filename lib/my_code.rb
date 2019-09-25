a056d04c98940e1f39358da60c2fcb1671f7b5ff
Tariq Bailey <tariqbailey627@gmail.com>
2019-09-25 00:32:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,8 +10,8 @@ end
 
 def reduce(source_array)
   i = 0 
-  total =0
-  while source_array.count > i do
+  total = 0
+  while i < source_array.count do
     total << yield(source_array[i])
     i += 1 
   end
